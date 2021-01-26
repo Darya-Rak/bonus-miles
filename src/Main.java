a6fb9f8c1dcff5d38e53c8c19d9975b220db5363
Darya-Rak <Darya-Rak@users.noreply.github.com>
2021-01-26 13:20:30 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float amount=4000F;
+        float percent=0.2F;
+        float bonus= amount * percent;
+        System.out.println(bonus);
+    }
+}
